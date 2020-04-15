1232765e25faea68244f66a203728631a7893cd4
Chris Otto <chrisotto@northwesternmutual.com>
2020-04-15 14:19:07 -0500

    Completed 4.5

--- steps/ResultsPage/then.js
+++ steps/ResultsPage/then.js
@@ -3,6 +3,5 @@ import { Then } from "cucumber";
 import verifyLinksContain from "../../support/assertions/verifyLinksContain"
 
 Then(/^links related to "(.*)" are shown on the results page$/, keyword => {
-  const links = $$(".LC20lb");
-  verifyLinksContain(links, keyword);
+  verifyLinksContain(keyword);
 });
--- support/actions/search.js
+++ support/actions/search.js
@@ -1,14 +1,9 @@
+import homePage from "../../pages/Home";
 /**
  * Search for a keyword
  * 
  * @param {String} keyword keyword to search for
- * @param {object} inputField WebDriverIO input field
- * @param {object} submitButton WebDriverIO submit button
  */
-export default (keyword, inputField, submitButton) => {
-    inputField.waitForDisplayed(5000);
-    inputField.click();
-    inputField.setValue(keyword);
-    submitButton.waitForDisplayed(5000);
-    submitButton.click();
+export default keyword => {
+    homePage.search(keyword);
 };
